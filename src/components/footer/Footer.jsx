beffd13918af652f3247db813be78263bae422c1
fendishatech <liyukibir@gmail.com>
2023-06-15 07:38:10 -0700

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,73 @@
+import {
+  AiFillInstagram,
+  AiFillFacebook,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-[8px] grid grid-cols-5 m-auto items-start justify-center">
+      {/* Logo */}
+      <div>
+        <div className="">
+          <h1 className="text-[25px] pb-[1.5rem] text-white">
+            <strong>Hello</strong>Logo
+          </h1>
+        </div>
+        <p className="text-white pb-[13px] opacity-70 leading-7">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, ut!
+        </p>
+      </div>
+      {/* Company */}
+      <div className="grid">
+        <span className="text-[18px] font-semibold pb-[1.5rem] text-white">
+          Company
+        </span>
+        <div className="grid gap-3 text-white">
+          <li className="list-none opacity-[.7] hover:opacity-[1]">About US</li>
+          <li className="list-none opacity-[.7] hover:opacity-[1]">Features</li>
+          <li className="list-none opacity-[.7] hover:opacity-[1]">News</li>
+          <li className="list-none opacity-[.7] hover:opacity-[1]">FAQ</li>
+        </div>
+      </div>
+      {/* Contact */}
+      <div className="grid">
+        <span className="text-[18px] font-semibold pb-[1.5rem] text-white">
+          Contact
+        </span>
+        <div className="grid gap-3 text-white">
+          <li className="list-none opacity-[.7] hover:opacity-[1]">About US</li>
+          <li className="list-none opacity-[.7] hover:opacity-[1]">Features</li>
+          <li className="list-none opacity-[.7] hover:opacity-[1]">News</li>
+          <li className="list-none opacity-[.7] hover:opacity-[1]">FAQ</li>
+        </div>
+      </div>
+      {/* Support */}
+      <div className="grid">
+        <span className="text-[18px] font-semibold pb-[1.5rem] text-white">
+          Support
+        </span>
+        <div className="grid gap-3 text-white">
+          <li className="list-none opacity-[.7] hover:opacity-[1]">About US</li>
+          <li className="list-none opacity-[.7] hover:opacity-[1]">Features</li>
+          <li className="list-none opacity-[.7] hover:opacity-[1]">News</li>
+          <li className="list-none opacity-[.7] hover:opacity-[1]">FAQ</li>
+        </div>
+      </div>
+      {/* Contact Info and social links */}
+      <div className="grid">
+        <span className="text-[18px] font-semibold pb-[1.5rem] text-white">
+          Contact Info
+        </span>
+        <small className="text-[14px] text-white">someCollEmail@mail.com</small>
+        <div className="flex gap-4 py-[1rem]">
+          <AiFillInstagram className="text-white text-[3rem] cursor-pointer" />{" "}
+          <AiFillFacebook className="text-white text-[3rem] cursor-pointer" />{" "}
+          <AiFillTwitterCircle className="text-white text-[3rem] cursor-pointer" />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/values/Values.jsx
+++ src/components/values/Values.jsx
@@ -1,17 +1,79 @@
 const Values = () => {
   return (
-    <div className="mt-[6rem] mb-[4rem]">
+    <section className="mt-[6rem] mb-[4rem]">
       <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px] block">
         The values that holds us true and to account
       </h1>
       <div className="grid gap-[10rem] grid-cols-3 items-center">
         <div className="rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
           <div className="flex items-center gap-3 ">
-            <div className="img"></div>
+            <div className="img p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+              <img
+                src="https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456"
+                alt=""
+                className="w-[70%]"
+              />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
           </div>
+          <p className="text-[16px] text-textColor opacity-[.7]">
+            Things made beautiful simpl are at the heart of everything.
+          </p>
+        </div>
+
+        <div className="rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3 ">
+            <div className="img p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+              <img
+                src="https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456"
+                alt=""
+                className="w-[70%]"
+              />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[16px] text-textColor opacity-[.7]">
+            Things made beautiful simpl are at the heart of everything.
+          </p>
+        </div>
+
+        <div className="rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3 ">
+            <div className="img p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+              <img
+                src="https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456"
+                alt=""
+                className="w-[70%]"
+              />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[16px] text-textColor opacity-[.7]">
+            Things made beautiful simpl are at the heart of everything.
+          </p>
         </div>
       </div>
-    </div>
+      {/* cta section */}
+      <div className="mt-[2rem] flex justify-between bg-slate-300 p-[5rem] rounded-[10px]">
+        <div>
+          <h1 className="text-white text-[30px] font-bold">
+            Ready to get started ?
+          </h1>
+          <h2 className="text-textColor text-[25px] font-bold">
+            Lets get started here
+          </h2>
+        </div>
+        <button className="border-[2px] border-blueColor rounded-[10px] px-[50px] py-[4px] text-[18px] font-semibold text-blueColor hover:bg-blueColor hover:text-white">
+          Get Started
+        </button>
+      </div>
+    </section>
   );
 };
 
